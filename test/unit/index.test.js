6f40a88849b21b7517bc7cde7372f8d3f2aad538
smlsunxie <smlsun@gmail.com>
2018-07-18 11:20:08 +0800

    完成使用者姓名驗證

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -15,7 +15,7 @@ describe('unit test', function () {
 
   it('display your name', function () {
     var config = require('./../../config/config');
-    expect(config.username).to.be.equal("yourname");
+    expect(config.username).to.be.equal("shan");
     require("../../dojo/dojo_start");
   });
 
